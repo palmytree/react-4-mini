94e86f3015fecc40d69f54154bed8f2abf102c19
palmytree <jeff@palmytree.com>
2020-05-28 13:33:21 -0500

    finished mini

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,26 @@
-import React, { Component } from "react";
-import logo from "./../../images/dm_white_logo.png";
-import "./Nav.css";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from './../../images/dm_white_logo.png'
+import './Nav.css'
 
 class Nav extends Component {
-  render() {
-    return (
-      <div className="Nav">
-        <div>
-          <img src={logo} alt="dm logo" />
-        </div>
-        <ul>
-          <li>Home</li>
-          <li>Topics</li>
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='Nav'>
+				<div>
+					<img src={logo} alt='dm logo' />
+				</div>
+				<ul>
+					<Link to='/'>
+						<li>Home</li>
+					</Link>
+					<Link to='/topics'>
+						<li>Topics</li>
+					</Link>
+				</ul>
+			</div>
+		)
+	}
 }
 
-export default Nav;
+export default Nav
